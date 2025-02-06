3f06618b68e8789b203d82a1e0b7716c04334998
Wilber Pierre Sanchez <wrpierre.bim.arqinge@gmail.com>
2025-02-06 08:41:12 -0500

    App

---
+++ src/main.ts
@@ -0,0 +1,82 @@
+import * as THREE from "three";
+import * as OBC from "@thatopen/components";
+import * as OBCF from "@thatopen/components-front";
+import Stats from "stats.js";
+//import * as BUI from "@thatopen/ui";
+//import { GUI } from "dat.gui";
+import { IfcLoaderModule } from "./IFCLoader";
+//import * as BUI from "@thatopen/ui";
+//import * as WEBIFC from "web-ifc";
+
+// Get the container element
+const container = document.getElementById("container");
+if (!container) {
+  throw new Error("Container element not found");
+}
+
+// Initialize components
+const components = new OBC.Components();
+
+// Create a new world
+const worlds = components.get(OBC.Worlds);
+
+const world = worlds.create<
+  OBC.SimpleScene,
+  OBC.OrthoPerspectiveCamera,
+  OBC.SimpleRenderer
+>();
+
+// Setup the world
+world.scene = new OBC.SimpleScene(components);
+world.renderer = new OBC.SimpleRenderer(components, container);
+world.camera = new OBC.OrthoPerspectiveCamera(components);
+
+// Initialize components
+components.init();
+
+// Set the camera to orthographic
+world.camera.projection.set("Orthographic");
+world.camera.set("Plan");
+world.camera.controls.setLookAt(0, 1, 0, 0, 0, 0);
+world.camera.three.position.y = 10;
+
+
+// Set the camera limits
+const position = new THREE.Vector3();
+world.camera.controls.getPosition(position);
+console.log(position);
+
+//Camera, renderer setup and background color
+world.scene.setup();
+world.scene.three.background = new THREE.Color(0x000000);
+
+//Add a grid
+const grids = components.get(OBC.Grids);
+grids.create(world);
+
+//Axes
+const axes = new THREE.AxesHelper(5);
+world.scene.three.add(axes);
+
+// Load a model
+const ifcLoaderModule = new IfcLoaderModule(components, world);
+await ifcLoaderModule.initialize();
+ifcLoaderModule.setupFileInput();
+
+// Highlighter
+const highlighter = components.get(OBCF.Highlighter);
+highlighter.setup({ world });
+highlighter.zoomToSelection = true;
+
+//BUI
+//const panel = new BUI.Panel();
+//document.body.append(panel);
+
+//Stats
+const stats = new Stats();
+stats.showPanel(2);
+document.body.append(stats.dom);
+stats.dom.style.left = "0px";
+stats.dom.style.zIndex = "unset";
+world.renderer.onBeforeUpdate.add(() => stats.begin());
+world.renderer.onAfterUpdate.add(() => stats.end());
