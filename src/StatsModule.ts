43eb99d156f4d6944f98d1561b43c0213364dbc5
Wilber Pierre Sanchez <wrpierre.bim.arqinge@gmail.com>
2025-02-08 10:35:47 -0500

    Create Stats module

---
+++ src/StatsModule.ts
@@ -0,0 +1,19 @@
+import Stats from "stats.js";
+import { SimpleRenderer } from "@thatopen/components";
+
+export class StatsModule {
+  private stats: Stats;
+
+  constructor() {
+    this.stats = new Stats();
+    this.stats.showPanel(2); // Show the FPS panel
+    document.body.append(this.stats.dom);
+    this.stats.dom.style.left = "0px";
+    this.stats.dom.style.zIndex = "unset";
+  }
+
+  public attachToRenderer(renderer: SimpleRenderer): void {
+    renderer.onBeforeUpdate.add(() => this.stats.begin());
+    renderer.onAfterUpdate.add(() => this.stats.end());
+  }
+}
