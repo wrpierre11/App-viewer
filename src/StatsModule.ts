e33564239e9c4956f0712612e9cb77cf7c363f3f
Wilber Pierre Sanchez <wrpierre.bim.arqinge@gmail.com>
2025-03-02 08:19:44 -0500

    Move down

--- src/StatsModule.ts
+++ src/StatsModule.ts
@@ -8,9 +8,14 @@ export class StatsModule {
     this.stats = new Stats();
     this.stats.showPanel(2); // Show the FPS panel
     document.body.append(this.stats.dom);
+
+    // Position the Stats panel at the left bottom
+    this.stats.dom.style.position = "fixed";
+    this.stats.dom.style.right = "10px";
+    this.stats.dom.style.bottom = "10px";
     this.stats.dom.style.left = "auto";
-    this.stats.dom.style.right = "0px";
-    this.stats.dom.style.zIndex = "unset";
+    this.stats.dom.style.top = "auto";
+    this.stats.dom.style.zIndex = "1000";    
   }
 
   public attachToRenderer(renderer: SimpleRenderer): void {
